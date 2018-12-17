41965db25108c74bdffb567ad38e401362556d75
Nia Tomonaka <nia1012-tmnk@outlook.jp>
2018-12-17 16:35:16 +0900

    ロボットによるアクセスはHTTP403で遮断

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -6,12 +6,27 @@ class MY_Controller extends CI_Controller {
 	public function __construct() {
 		parent::__construct();
 		$this->load->model( 'usermodel' );
+		$this->load->library( 'user_agent' );
+		$this->reject_robot_access();
 	}
 	
+	/**
+	 * ログインしていない場合、リダイレクトURLを設定したログインページにリダイレクトします。
+	 */
 	protected function redirect_if_not_login( $redirect_to ) {
 		if( !UserModel::is_login() ) {
             redirect( "/login?redirect=apps/{$redirect_to}" );
         }
+	}
+
+	/**
+	 * ロボットによるアクセスの時、HTTP403を返してリジェクトします。
+	 */
+	protected function reject_robot_access() {
+		if( $this->agent->is_robot() ) {
+			set_status_header( 403 );
+			exit();
+		}
 	}
 }
 
